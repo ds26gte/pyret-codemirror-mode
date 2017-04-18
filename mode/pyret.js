c8ac32c8dd043ab8ce34ff9dc79c6f8a4924ce7c
Dorai Sitaram <ds26gte@yahoo.com>
2017-04-18 11:38:09 -0400

    Make \b legal inside Pyret strings.

--- mode/pyret.js
+++ mode/pyret.js
@@ -146,14 +146,14 @@ CodeMirror.defineMode("pyret", function(config, parserConfig) {
                  "\\\\[01234567]{1,3}" +
                  "|\\\\x[0-9a-fA-F]{1,2}" +
                  "|\\\\u[0-9a-fA-f]{1,4}" +
-                 "|\\\\[\\\\nrt\"\']" +
+                 "|\\\\[\\\\bnrt\"\']" +
                  "|[^\\\\\"\n\r])*\"");
     const squot_str =
       new RegExp("^\'(?:" +
                  "\\\\[01234567]{1,3}" +
                  "|\\\\x[0-9a-fA-F]{1,2}" +
                  "|\\\\u[0-9a-fA-f]{1,4}" +
-                 "|\\\\[\\\\nrt\"\']" +
+                 "|\\\\[\\\\bnrt\"\']" +
                  "|[^\\\\\'\n\r])*\'");
     const unterminated_string = new RegExp("^[\"\'].*");
 
